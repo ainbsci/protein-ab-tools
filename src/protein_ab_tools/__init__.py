c77ac6e436aff8e93b244a278cc992f6d8a0431e
Seung-woo Seo <sw32.seo@gmail.com>
2024-12-02 14:48:55 +0900

    Refactor imports in __init__.py to improve module structure
    - Updated import paths in the __init__.py file of the protein_ab_tools package to reflect the new module organization, specifically importing functions from the ab_analysis.numbering and align.sequence_align submodules for better clarity and accessibility.

--- src/protein_ab_tools/__init__.py
+++ src/protein_ab_tools/__init__.py
@@ -1,2 +1,2 @@
-from .ab_analysis import ab_numbering, extract_regions
-from .align import calc_percent_similarity
+from .ab_analysis.numbering import ab_numbering, extract_regions
+from .align.sequence_align import calc_percent_similarity
