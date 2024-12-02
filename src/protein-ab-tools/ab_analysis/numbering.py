8cd6021c2868c2a994d913014dc68371711b7148
Seung-woo Seo <sw32.seo@gmail.com>
2024-12-02 14:19:50 +0900

    Add extract_regions function to number antibody sequence regions
    - Implemented extract_regions function in `numbering.py` to extract specific regions (FWRs and CDRs) from antibody sequences based on IMGT and AHO numbering schemes.
    - Added breakpoint definitions for both schemes and included error handling for invalid schemes.
    - Included main block with assertions to validate the functionality against expected outputs for heavy and light chains.

--- src/protein-ab-tools/ab_analysis/numbering.py
+++ src/protein-ab-tools/ab_analysis/numbering.py
@@ -22,3 +22,74 @@ def numbering(seq: str,
         raise ValueError(f"Invalid sequence: {seq}")
 
     return result
+
+
+def extract_regions(seq: str,
+                    scheme: str = 'imgt',
+                    chain: Literal['H', 'L'] = 'H'):
+    """
+    Extract regions from an antibody sequence.
+    """
+    result = numbering(seq, scheme=scheme, chain=chain)
+    if scheme.lower() == 'imgt':
+        breakpoint = {
+            'fwr1': [1, 27],
+            'cdr1': [27, 39],
+            'fwr2': [39, 56],
+            'cdr2': [56, 66],
+            'fwr3': [66, 105],
+            'cdr3': [105, 118],
+            'fwr4': [118, 129]
+        }
+    elif scheme.lower() == 'aho':
+        # https://plueckthun.bioc.uzh.ch/antibody/Numbering/NumFrame.html
+        breakpoint = {
+            'fwr1': [1, 27],
+            'cdr1': [27, 41],
+            'fwr2': [41, 58],
+            'cdr2': [58, 69],
+            'fwr3': [69, 107],
+            'cdr3': [107, 139],
+            'fwr4': [139, 150] if chain == 'H' else [139, 149]
+        }
+    else:
+        raise ValueError('Invalid numbering scheme')
+    chain = 'vh' if chain == 'H' else 'vl'
+    numbered_seq = result[0][0][0][0]
+    regions = {f'{chain}_{k}': '' for k in breakpoint.keys()}
+    for (number, _), aa in numbered_seq:
+        region = next(
+            (k for k, v in breakpoint.items() if v[0] <= number < v[1]), None)
+        if region is not None:
+            regions[f'{chain}_{region}'] += aa
+    return regions
+
+
+if __name__ == '__main__':
+
+    right_h = {
+        'vh_fwr1': 'QVQLVESGG-GVVQPGRSLRLDCKAS',
+        'vh_cdr1': 'GITF----SNSG',
+        'vh_fwr2': 'MHWVRQAPGKGLEWVAV',
+        'vh_cdr2': 'IWYD--GSKR',
+        'vh_fwr3': 'YYADSVK-GRFTISR-NSKNTLFLQMNSLRAEDTAVYYC',
+        'vh_cdr3': 'ATN-------DDY',
+        'vh_fwr4': 'WGQGTLVTTVSS'
+    }
+    right_l = {
+        'vl_fwr1': 'EIVLTQSPATLSLSPGERATLSCRAS',
+        'vl_cdr1': 'QSV------SGY',
+        'vl_fwr2': 'LAWYQQKPGQAPRLLIY',
+        'vl_cdr2': 'DA-------S',
+        'vl_fwr3': 'NRATGIP-ARFSGSG--SGTDFTLTISSLEPEDFAVYYC',
+        'vl_cdr3': 'QQSSN----WPRT',
+        'vl_fwr4': 'FGQGTKVEIK'
+    }
+    expected_h = extract_regions(
+        'QVQLVESGGGVVQPGRSLRLDCKASGITFSNSGMHWVRQAPGKGLEWVAVIWYDGSKRYYADSVKGRFTISRNSKNTLFLQMNSLRAEDTAVYYCATNDDYWGQGTLVTTVSS',
+        chain='H')
+    assert expected_h == right_h
+    expected_l = extract_regions(
+        'EIVLTQSPATLSLSPGERATLSCRASQSVSGYLAWYQQKPGQAPRLLIYDASNRATGIPARFSGSGSGTDFTLTISSLEPEDFAVYYCQQSSNWPRTFGQGTKVEIK',
+        chain='L')
+    assert expected_l == right_l
