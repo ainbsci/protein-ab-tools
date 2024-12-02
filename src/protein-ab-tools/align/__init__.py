9130ac6294a26b07e2824e196f0ea343db9bb9e4
Seung-woo Seo <sw32.seo@gmail.com>
2024-12-02 13:28:00 +0900

    Add initial alignment functionality and requirements
    - Created `requirements.txt` to include Biopython as a dependency.
    - Added `numbering` function in `__init__.py` for sequence numbering using ANARCI.
    - Implemented `calc_percent_similarity` function in `sequence_align.py` to compute percent similarity between protein sequences.
    - Included a main block for testing the similarity calculation with example sequences.

---
+++ src/protein-ab-tools/align/__init__.py
@@ -0,0 +1,21 @@
+from typing import Literal, Optional
+from anarci import anarci
+
+
+def numbering(seq: str,
+              name: Optional[str] = None,
+              scheme: str = 'imgt',
+              chain: Literal['H', 'L'] = 'H'):
+    if name is None:
+        name = f'{chain}-{scheme}'
+    prep_seq = (name, seq)
+    if chain == 'H':
+        chain = ['H']
+    elif chain == 'L':
+        chain = ['K', 'L']
+    result = anarci([prep_seq], scheme=scheme, allow=chain)
+
+    if result[0][0] is None:
+        raise ValueError(f"Invalid sequence: {seq}")
+
+    return result
