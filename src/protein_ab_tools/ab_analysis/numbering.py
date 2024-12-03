b8a815cae304e368fddb5195dc87796011291d56
Seung-woo Seo <sw32.seo@gmail.com>
2024-12-03 20:53:59 +0900

    Update .gitignore and refine breakpoint definitions in `numbering.py` for antibody sequence processing
    - Removed PyCharm specific comments from .gitignore and added a rule to ignore Jupyter Notebook files.
    - Adjusted breakpoint ranges for IMGT and AHO numbering schemes in `numbering.py` for improved accuracy.
    - Introduced breakpoint definitions for CHOTHIA and KABAT numbering schemes to enhance functionality.

--- src/protein_ab_tools/ab_analysis/numbering.py
+++ src/protein_ab_tools/ab_analysis/numbering.py
@@ -41,24 +41,44 @@ def extract_regions(seq: str,
     result = run_numbering(seq, scheme=scheme, chain=chain)
     if scheme.lower() == 'imgt':
         breakpoint = {
-            'fwr1': [1, 27],
-            'cdr1': [27, 39],
-            'fwr2': [39, 56],
-            'cdr2': [56, 66],
-            'fwr3': [66, 105],
-            'cdr3': [105, 118],
-            'fwr4': [118, 129]
+            'fwr1': [1, 26],
+            'cdr1': [27, 38],
+            'fwr2': [39, 55],
+            'cdr2': [56, 65],
+            'fwr3': [66, 104],
+            'cdr3': [105, 117],
+            'fwr4': [118, 128]
         }
     elif scheme.lower() == 'aho':
         # https://plueckthun.bioc.uzh.ch/antibody/Numbering/NumFrame.html
         breakpoint = {
-            'fwr1': [1, 27],
-            'cdr1': [27, 41],
-            'fwr2': [41, 58],
-            'cdr2': [58, 69],
-            'fwr3': [69, 107],
-            'cdr3': [107, 139],
-            'fwr4': [139, 150] if chain == 'H' else [139, 149]
+            'fwr1': [1, 26],
+            'cdr1': [27, 40],
+            'fwr2': [41, 57],
+            'cdr2': [58, 68],
+            'fwr3': [69, 106],
+            'cdr3': [107, 138],
+            'fwr4': [139, 149] if chain == 'H' else [139, 148]
+        }
+    elif scheme.lower() == 'chothia':
+        breakpoint = {
+            'fwr1': [1, 25] if chain == 'H' else [1, 23],
+            'cdr1': [26, 32] if chain == 'H' else [24, 34],
+            'fwr2': [33, 49] if chain == 'H' else [35, 49],
+            'cdr2': [52, 56] if chain == 'H' else [50, 56],
+            'fwr3': [63, 94] if chain == 'H' else [57, 88],
+            'cdr3': [95, 102] if chain == 'H' else [89, 97],
+            'fwr4': [103, 146] if chain == 'H' else [98, 146]
+        }
+    elif scheme.lower() == 'kabat':
+        breakpoint = {
+            'fwr1': [1, 30] if chain == 'H' else [1, 23],
+            'cdr1': [31, 35] if chain == 'H' else [24, 34],
+            'fwr2': [36, 49] if chain == 'H' else [35, 49],
+            'cdr2': [50, 65] if chain == 'H' else [50, 56],
+            'fwr3': [64, 94] if chain == 'H' else [57, 88],
+            'cdr3': [95, 102] if chain == 'H' else [89, 97],
+            'fwr4': [103, 146] if chain == 'H' else [98, 146]
         }
     else:
         raise ValueError('Invalid numbering scheme')
@@ -67,7 +87,7 @@ def extract_regions(seq: str,
     regions = {f'{chain}_{k}': '' for k in breakpoint.keys()}
     for (number, _), aa in numbered_seq:
         region = next(
-            (k for k, v in breakpoint.items() if v[0] <= number < v[1]), None)
+            (k for k, v in breakpoint.items() if v[0] <= number <= v[1]), None)
         if region is not None:
             regions[f'{chain}_{region}'] += aa
     return regions
